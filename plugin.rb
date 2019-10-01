e92aab04640d24f90dbab420532ac8a21319ae0b
Angus McLeod <angus@mcleod.org.au>
2019-10-01 20:29:17 +1000

    Ensure admins can edit usernames

--- plugin.rb
+++ plugin.rb
@@ -106,7 +106,7 @@ after_initialize do
   
   module GuardianWikimediaExtension
     def can_edit_username?(user)
-      return false if SiteSetting.wikimedia_auth_enabled
+      return false if !is_admin? && SiteSetting.wikimedia_auth_enabled
       super(user)
     end
   end
